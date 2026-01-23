baf146a3237b3b07cfd2dfe4161513ab473fd38d
DEVducanh <nguyenanh191025@gmail.com>
2026-01-23 16:39:16 +0700

    fix status table

--- src/sessions/sessions.service.ts
+++ src/sessions/sessions.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Session } from './schemas/session.schema';
-import { SESSION_STATUS } from '../common/enums/enum';
+import { Table } from '../tables/schemas/table.schema';
+import { SESSION_STATUS, TABLE_STATUS } from '../common/enums/enum';
 
 @Injectable()
 export class SessionsService {
   constructor(
     @InjectModel(Session.name)
     private readonly sessionModel: Model<Session>,
+    @InjectModel(Table.name)
+    private readonly tableModel: Model<Table>,
   ) {}
 
   async findActiveSession(tableId: string | Types.ObjectId) {
@@ -30,11 +33,16 @@ export class SessionsService {
       started_at: new Date(),
     });
 
+    // Cập nhật trạng thái bàn thành UNAVAILABLE
+    await this.tableModel.findByIdAndUpdate(tableId, {
+      status: TABLE_STATUS.UNAVAILABLE,
+    });
+
     return newSession;
   }
 
   async closeSession(sessionId: string) {
-    return this.sessionModel.findByIdAndUpdate(
+    const session = await this.sessionModel.findByIdAndUpdate(
       sessionId,
       {
         status: SESSION_STATUS.CLOSED,
@@ -42,5 +50,13 @@ export class SessionsService {
       },
       { new: true },
     );
+
+    if (session) {
+      await this.tableModel.findByIdAndUpdate(session.table_id, {
+        status: TABLE_STATUS.AVAILABLE,
+      });
+    }
+
+    return session;
   }
 }
